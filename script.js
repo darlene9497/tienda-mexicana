482bed2131bc5592be94301bff5031f84d16a56c
darlene9497 <darlene.nandabi@student.moringaschool.com>
2024-11-08 10:57:43 +0300

    :sparkles: added the testimonials section

--- script.js
+++ script.js
@@ -11,3 +11,34 @@ menuOpenButton.addEventListener("click", () => {
 menuCloseButton.addEventListener("click", () => {
     document.body.classList.remove("menu-open");
 });
+
+// Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 25,
+    
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        }
+    }
+});
